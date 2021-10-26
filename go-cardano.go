aedab6f87f9147dc1324e992e75bbe75c8ad2fb7
devhulk <gerald.yerden@gmail.com>
2021-10-26 23:05:28 +0000

    multiple wallet test

--- go-cardano.go
+++ go-cardano.go
@@ -5,14 +5,16 @@ import (
     "fmt"
 )
 
-// type Wallet {}
-
-// type Transaction {}
 
 func main() {
 
     w := wallet.CreateWallet("GeraldWallet")
+    w2 := wallet.CreateWallet("GeraldWallet2")
     fmt.Println(w.Name)
     fmt.Println(w.PaymentAddr)
     fmt.Println(w.Location)
+
+    fmt.Println(w2.Name)
+    fmt.Println(w2.PaymentAddr)
+    fmt.Println(w2.Location)
 }
